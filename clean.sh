9d3d2f204aa6d60a1cb6957061f18e1f2e1fda39
david <david3oliveros@gmail.com>
2023-11-09 22:34:48 -0500

    initial commit

---
+++ clean.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+cd out/build ; rm -rf App.data App.js App.wasm App.html CMakeCache.txt Makefile CMakeFiles cmake_install.cmake
---
+++ run.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+cd out/build ; ./App
+
+#cd out/build ; emrun --browser google-chrome-stable App.html
