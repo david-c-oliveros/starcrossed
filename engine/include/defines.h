a71b3b2a7d128a6f422a8a14bbf93bb2189a13b0
david <david3oliveros@gmail.com>
2023-11-11 17:17:36 -0500

    tweak

--- engine/include/defines.h
+++ engine/include/defines.h
@@ -10,7 +10,7 @@
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
 #define PLATFORM_WINDOWS 1
 #ifndef _WIN64
-#error "64-bit is required on Windows"
+#error 64-bit is required on Windows
 #endif
 
 #elif defined(__linux__) || defined(__gnu_linux)
