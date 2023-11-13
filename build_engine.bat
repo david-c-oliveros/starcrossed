f1f150387018a26c1c4c014a972821c929bcde88
david <david3oliveros@gmail.com>
2023-11-13 15:10:35 -0500

    batch file fix

--- build_engine.bat
+++ build_engine.bat
@@ -20,8 +20,10 @@ REM -Wall -Werror
 SET includeFlags=-Iengine/include -Igame/include -Iexternal/glad/include/ -Iexternal/glfw-3.3.8/include/ -Iexternal/glm
 
 SET linkerFlags=^
--luser32^
--Lexternal/glfw/build/src -lglfw
+ -luser32^
+ -Lexternal/glfw-3.3.8/build/windows/src -lglfw3^
+ -lgdi32^
+ -lopengl32
 
 SET defines=-D_DEBUG
 
